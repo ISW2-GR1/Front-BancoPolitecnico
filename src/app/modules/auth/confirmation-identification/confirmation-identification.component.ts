1516dc4ba506116303f402c4da4a93499294d9b4
Miguel-EMC <eduardomuzo123456@gmail.com>
2024-07-28 21:48:19 -0500

    Cambios

--- src/app/modules/auth/confirmation-identification/confirmation-identification.component.ts
+++ src/app/modules/auth/confirmation-identification/confirmation-identification.component.ts
@@ -3,14 +3,14 @@ import { RouterLink } from '@angular/router';
 import { fuseAnimations } from '@fuse/animations';
 
 @Component({
-    selector     : 'confirmation-required-modern-reversed',
+    selector     : 'auth-confirmation-required',
     templateUrl  : './confirmation-required.component.html',
     encapsulation: ViewEncapsulation.None,
     animations   : fuseAnimations,
     standalone   : true,
     imports      : [RouterLink],
 })
-export class ConfirmationRequiredModernReversedComponent
+export class AuthConfirmatIdentificationComponent
 {
     /**
      * Constructor
