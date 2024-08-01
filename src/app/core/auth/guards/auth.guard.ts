9e219514a10838e4a7f7ab6064f60357b766aed0
Miguel-EMC <eduardomuzo123456@gmail.com>
2024-08-01 00:06:36 -0500

    Update

--- src/app/core/auth/guards/auth.guard.ts
+++ src/app/core/auth/guards/auth.guard.ts
@@ -7,21 +7,16 @@ export const AuthGuard: CanActivateFn | CanActivateChildFn = (route, state) =>
 {
     const router: Router = inject(Router);
 
-    // Check the authentication status
     return inject(AuthService).check().pipe(
         switchMap((authenticated) =>
         {
-            // If the user is not authenticated...
             if ( !authenticated )
             {
-                // Redirect to the sign-in page with a redirectUrl param
                 const redirectURL = state.url === '/sign-out' ? '' : `redirectURL=${state.url}`;
                 const urlTree = router.parseUrl(`sign-in?${redirectURL}`);
 
                 return of(urlTree);
             }
-
-            // Allow the access
             return of(true);
         }),
     );
